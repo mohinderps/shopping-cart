a6026e53225d0a7471b9f1f18816b43b79235b4e
mohinderps <mohin19derp@gmail.com>
2018-07-20 21:37:35 +0530

    Added Order Summary Header

--- src/Components/CartDetails/index.js
+++ src/Components/CartDetails/index.js
@@ -7,6 +7,11 @@ const CartDetails = (props) => {
   return (
     <div className="cart-details">
       <ul className="cart-items-list">
+        <li className="cart-list-item">
+          <div className="cart-header-name">Items</div>
+          <div className="cart-header-quantity">Quantity</div>
+          <div className="cart-header-price">Price</div>
+        </li>
         {cart.map(item => <CartItem key={item.id} item={item} />)}
       </ul>
     </div>
